1203ddc5b4d5c24e9c8ddf46bb14867f97b80e34
Josue Colin <josue230905@gmail.com>
2026-08-27 14:06:15 -0600

    Completa operaciones basicas de pila

--- pila/Pila.cpp
+++ pila/Pila.cpp
@@ -17,6 +17,11 @@ void inicializar(Pila *p)
     p->tope = nullptr;
 }
 
+bool vacia(Pila *p)
+{
+    return p->tope == nullptr;
+}
+
 void push(Pila *p, int x)
 {
     Nodo *nuevo = new Nodo;
@@ -27,12 +32,40 @@ void push(Pila *p, int x)
     p->tope = nuevo;
 }
 
-void pop(Pila *p)
+int pop(Pila *p)
 {
+    if (vacia(p))
+    {
+        cout << "La pila esta vacia:" << endl;
+        return -1;
+    }
+
     Nodo *a = p->tope;
     int x = a->dato;
     p->tope = a->sig;
-    delete (a);
+    delete a;
+    return x;
+}
+
+int peek(Pila *p)
+{
+    if (vacia(p))
+    {
+        cout << "La pila esta vacia:" << endl;
+        return -1;
+    }
+
+    return p->tope->dato;
+}
+
+void mostrar(Pila *p)
+{
+    cout << "La pila contiene: ";
+    for (Nodo *a = p->tope; a; a = a->sig)
+    {
+        cout << a->dato << " -> ";
+    }
+    cout << "NULL" << endl;
 }
 
 int main()
@@ -40,10 +73,15 @@ int main()
     Pila p;
 
     inicializar(&p);
-
     push(&p, 10);
     push(&p, 20);
     push(&p, 30);
 
+    mostrar(&p);
+    cout << "El tope actual es: " << peek(&p) << endl;
+    int eliminado = pop(&p);
+    cout << "Se elimino: " << eliminado << endl;
+    mostrar(&p);
+    cout << "El nuevo tope es: " << peek(&p) << endl;
     return 0;
 }
